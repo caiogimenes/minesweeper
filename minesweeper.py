eda7d34fbb08f062819cd7e1fa311555f10a05a3
caio <ciaonemegi@gmail.com>
2023-11-16 13:47:37 -0300

    main

--- minesweeper.py
+++ minesweeper.py
@@ -105,28 +105,37 @@ class Sentence():
         """
         Returns the set of all cells in self.cells known to be mines.
         """
-        raise NotImplementedError
+        if len(self.cells) == self.count:
+            return self.cells
+        return
 
     def known_safes(self):
         """
         Returns the set of all cells in self.cells known to be safe.
         """
-        raise NotImplementedError
+        if self.count == 0:
+            return self.cells
 
     def mark_mine(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be a mine.
         """
-        raise NotImplementedError
-
+        if cell not in self.cells:
+            return
+        else:
+            self.cells -= cell
+            self.count -= 1
+            
     def mark_safe(self, cell):
         """
         Updates internal knowledge representation given the fact that
         a cell is known to be safe.
         """
-        raise NotImplementedError
-
+        if cell not in self.cells:
+            return
+        else:
+            self.cells -= cell
 
 class MinesweeperAI():
     """
@@ -182,8 +191,16 @@ class MinesweeperAI():
             5) add any new sentences to the AI's knowledge base
                if they can be inferred from existing knowledge
         """
-        raise NotImplementedError
-
+        if cell in self.moves_made:
+            return
+        
+        self.moves_made(cell)
+        self.mark_safe(cell)
+        
+        sentence = Sentence()
+           
+        
+                
     def make_safe_move(self):
         """
         Returns a safe cell to choose on the Minesweeper board.
